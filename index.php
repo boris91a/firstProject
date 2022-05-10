8dfdba9c225a748936d97f6432499be2a1d59358
boris <boris91a@gmail.com>
2022-05-10 20:13:17 +0300

    Added multiple table

--- index.php
+++ index.php
@@ -2,16 +2,43 @@
 <html lang="RU">
 <head>
     <title></title>
+<style>
+table {
+    border-collapse: collapse;
+}
+
+table td {
+    border: solid 2px;
+    padding: 10px;
+    line-height: 20px;
+}
+</style>
 </head>
 <body>
 <header></header>
 <nav>
     <ul>
-        <li><a href="#">Урок 1</a></li>
+        <li><a href="#multTbl">Урок 1</a></li>
         <li><a href="#">Урок 2</a></li>
     </ul>
 </nav>
-<main><p>Hello, <b>NIX Education</b></p></main>
+<main>
+    <p>Hello, <b>NIX Education</b></p>
+    <table id="multTbl">
+<?php
+    for($i=1; $i<=10; $i++){
+        if($i == 1) echo "<tr>";
+        echo "<td>";
+        for($j=1; $j<=10; $j++){
+            echo "$i x $j = ".$i*$j."</br>";
+        }
+        echo "</td>";
+        if($i == 5) echo "</tr><tr>";
+        if($i==10) echo "</tr>";
+    }
+?>
+    </table>
+</main>
 <footer></footer>
 </body>
 </html>
