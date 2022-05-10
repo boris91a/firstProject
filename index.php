9a2069398dd6294f3659b3e70d93c1ffa49dc175
boris <boris91a@gmail.com>
2022-05-10 19:51:30 +0300

    added structure and Menu (2 links)

--- index.php
+++ index.php
@@ -4,6 +4,14 @@
     <title></title>
 </head>
 <body>
+<header></header>
+<nav>
+    <ul>
+        <li><a href="#">Урок 1</a></li>
+        <li><a href="#">Урок 2</a></li>
+    </ul>
+</nav>
 <main><p>Hello, <b>NIX Education</b></p></main>
+<footer></footer>
 </body>
 </html>
